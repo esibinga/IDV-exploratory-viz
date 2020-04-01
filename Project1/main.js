e759c5f400d2fed5fcb4f8ffc2589dafc3fe6eaa
Eva Sibinga <evasibinga@MacBook-Pro-5.local>
2020-03-31 23:48:45 -0400

    3-31 udpate

--- Project1/main.js
+++ Project1/main.js
@@ -50,11 +50,11 @@ function init() {
 // + CREATE YOUR ROOT/NODE HIERARCHY NODE
     const root = d3
       //.hierarchy(state.data, d => d.children)
-      .hierarchy({ id: null, tree: state.data.nodes }, d => d.tree)
-      .count(); //set the data source
+      .hierarchy({ id: null, tree: state.data.nodes }, d => d.tree) //right now it looks like the 5 generations are levels of data for each person, not each svg corresponding to the "end point" of a genealogical path
+      .count(); //can I set the end point of the path manually? or semi-manually?
   
-    console.log("data:", state.data) //this looks good
-    console.log("root:", root) //this looks good
+    console.log("data:", state.data)
+    console.log("root:", root)
   
 // + CREATE YOUR LAYOUT GENERATOR
     const tree = d3
@@ -66,7 +66,7 @@ function init() {
     tree(root);
 
     // + CREATE YOUR GRAPHICAL ELEMENTS
-    const leaf = svg //*** .data looks like the source of the problem -- only one svg is created, not a group */
+    const leaf = svg
       .selectAll("g")
       .data(root.descendants()) //each svg is supposed to be the final descendant of root. Leaf (any node without children) seems like it should work too, but neither has worked yet
       .join("g")
@@ -77,12 +77,13 @@ function init() {
       .attr("fill", function(d) {
         var returnColor;
         var idString = new RegExp('@P\d\d\d@');
-        //console.log(d.data.tag);
+        //console.log(d.data);
         if (d.data.tag == "NAME") { returnColor = "blue";
-        //} else if (d.data.tag == "NAME") {returnColor = "orange";
-        } else if (d.data.id == '@P1@') {returnColor = "yellow"; //this also works, but it looks like @P##@ are all in the first generation
-        //} else if (d.data.id == '@P3@') {returnColor = "red";
-        } else {returnColor = "none"}
+        } else if (d.data.tag == "FAM") {returnColor = "red";
+        } else if (d.data.tag == "INDI") {returnColor = "purple";
+        } else if (d.data.id == '@P1@') {returnColor = "yellow"; // @P1@ is the first person in this tree -- makes sense to see it top left
+        } else if (d.data.value < 100) {returnColor = "orange"; // @P125@ is a person much farther down the free, but this dot is showing up farther along the gen. 1 row, not down a level or two
+        } else {returnColor = "green"}
         return returnColor;})
       .attr("r", 10)    
       .attr("width", d => d.x)
